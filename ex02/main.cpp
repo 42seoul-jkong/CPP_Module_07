9380adbff5ef3363ae85574774aa700df8a86da0
jkong <jkong@student.42seoul.kr>
2022-07-14 17:21:42 +0900

    인트라의 테스트 코드 포함

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,6 +7,8 @@
 #include <sstream>
 #include <string>
 
+#define MAX_VAL 750
+
 template <typename T>
 void iter(Array<T>& array, void (*pfn)(T&))
 {
@@ -35,7 +37,7 @@ static void print_string(const std::string& i)
     std::cout << "String: " << i << std::endl;
 }
 
-int main()
+int ft_main()
 {
     Array<int> empty;
 
@@ -68,4 +70,54 @@ int main()
     }
 
     return 0;
+}
+
+int main(int, char**)
+{
+    Array<int> numbers(MAX_VAL);
+    int* mirror = new int[MAX_VAL];
+    srand(time(NULL));
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        const int value = rand();
+        numbers[i] = value;
+        mirror[i] = value;
+    }
+    // SCOPE
+    {
+        Array<int> tmp = numbers;
+        Array<int> test(tmp);
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        if (mirror[i] != numbers[i])
+        {
+            std::cerr << "didn't save the same value!!" << std::endl;
+            return 1;
+        }
+    }
+    try
+    {
+        numbers[-2] = 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+    try
+    {
+        numbers[MAX_VAL] = 0;
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << '\n';
+    }
+
+    for (int i = 0; i < MAX_VAL; i++)
+    {
+        numbers[i] = rand();
+    }
+    delete[] mirror; //
+    return ft_main();
 }
